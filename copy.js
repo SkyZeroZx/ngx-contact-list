4f020eadf75a8144227c9d56fdb40dd8dee31f83
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2022-09-10 01:15:50 -0500

    Init Develop

--- copy.js
+++ copy.js
@@ -1,4 +1,4 @@
 let cpx = require("cpx2");
 
-cpx.copy('src/*.scss', 'ngx-contact-list');
-cpx.copy('README.md', 'ngx-contact-list');
+cpx.copy("src/*.scss", "ngx-contact-list");
+cpx.copy("README.md", "ngx-contact-list");
